c6ee53e803f7ff67fa5f56604e163f074a922d5d
wonhui-kim <0512tini@gmail.com>
2022-10-19 19:56:48 +0900

    K번째 수

--- Programmers/K번째수/main.swift
+++ Programmers/K번째수/main.swift
@@ -13,17 +13,14 @@ func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
         return array[arr[0]-1...arr[1]-1].sorted()[arr[2]-1]
     })
     
-    //result 변수를 선언하고 return 밖에 안해주기 때문에 선언하지 않고 바로 return해주는 코드가 위의 코드!
+    /*result 변수를 선언하고 return 밖에 안해주기 때문에 선언하지 않고 바로 return해주는 코드가 위의 코드!*/
 //    var result = commands.map ({ element in
 //        return array[element[0]-1...element[1]-1].sorted()[element[2]-1]
 //    })
 //
 //    return result
     
-    
-    
-    //기존 코드
-    
+    /*기존 코드*/
 //    var result = [Int]()
 //
 //    commands.forEach { element in
